80015dad9a51e459bd5e5f7e93bab359dfe7adab
Dairo Carrasquilla <dcarrasquilla@ises.com.co>
2026-03-09 14:49:09 -0500

    chore: maestro_db table modify 22060309.1429

--- scripts/script.sql
+++ scripts/script.sql
@@ -8,12 +8,6 @@ create table maestro_priorizacion_suspension (
 	fecha_registro timestamptz null default current_timestamp
 );
 
-select 
-	mps.prioridad, mps.detalle_prioridad, mps.alcance_accion_definida
-from maestro_priorizacion_suspension mps
-where mps.eliminado = false and mps.fecha_registro::date = current_date;
-
-
 create table maestro_db (
     id serial primary key,
     num_lote varchar null,
@@ -45,7 +39,7 @@ create table maestro_db (
     tipo_suspension_scr varchar null,
     estado_del_servicio varchar null,
     deuda_hoy decimal null,
-    fecha_de_vencimiento date null,
+    fecha_de_vencimiento int null,
     facturas_vencidas_2 int null,
     nombre_del_cliente_2 varchar null,
     tarifa_2 varchar null,
@@ -62,7 +56,6 @@ create table maestro_db (
     exclusiones_temporal varchar null,
     tipo_brigada varchar null,
     cluster varchar null,
-    zona_maestro varchar null,
     eliminado boolean default false,
     fecha_registro timestamptz default current_timestamp
 );
